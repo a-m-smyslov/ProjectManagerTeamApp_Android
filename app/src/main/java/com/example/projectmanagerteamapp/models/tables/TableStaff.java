f7f215cf9a970dc2319d630c48957dc835dce903
Alexei Smyslov <a.m.smyslov@yandex.ru>
2021-01-26 02:16:34 +0300

    add entities and dbmanager starting tables

---
+++ app/src/main/java/com/example/projectmanagerteamapp/models/tables/TableStaff.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagerteamapp.models.tables;
+
+import com.example.projectmanagerteamapp.models.tools.DbHelper;
+
+public class TableStaff
+{
+    private DbHelper dbHelper;
+
+    public TableStaff(DbHelper dbHelper)
+    {
+        this.dbHelper = dbHelper;
+    }
+}
